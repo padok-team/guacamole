8a9c69fe124d47102b760e65f708865ebdfaf670
Benjamin Sanvoisin <benjamin@sanvoisin.io>
2023-06-30 16:08:56 +0200

    feat(interface): checks

--- checks/checks.go
+++ checks/checks.go
@@ -2,6 +2,7 @@ package checks
 
 import (
 	"fmt"
+	"guacamole/data"
 	"os"
 	"strconv"
 
@@ -10,31 +11,30 @@ import (
 
 func Checks() {
 	fmt.Println("Guacamole is cooking 🥑")
-	totalChecks := 2
 	totalChecksOk := 0
-	// Checking the module for provider
-	check1 := ProviderInModule()
-	// Checkoing the naming of the resources
-	check2 := Naming()
-	//Calculating the score in percentage
-	if check1.Status == "✅" {
-		totalChecksOk++
+	// List of checks to perform
+	listOfChecks := []func() data.Check{
+		ProviderInModule,
+		Naming,
 	}
-	if check2.Status == "✅" {
-		totalChecksOk++
+	totalChecks := len(listOfChecks)
+	// Displaying the results
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "Check", "Status", "Related guidelines"})
+	for i, check := range listOfChecks {
+		c := check()
+		if c.Status == "✅" {
+			totalChecksOk++
+		}
+		t.AppendRows([]table.Row{
+			{i + 1, c.Name, c.Status, c.RelatedGuidelines},
+		})
 	}
 	score := strconv.Itoa(totalChecksOk*100/totalChecks) + "%"
 	if score == "100%" {
 		score = score + " 🎉"
 	}
-	// Displaying the results
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"#", "Check", "Status", "Related guidelines"})
-	t.AppendRows([]table.Row{
-		{1, check1.Name, check1.Status, check1.RelatedGuidelines},
-		{2, check2.Name, check2.Status, check2.RelatedGuidelines},
-	})
 	t.AppendFooter(table.Row{"", "", "Score", score})
 	t.Render()
 	fmt.Println("Your guacamole is ready 🥑")
