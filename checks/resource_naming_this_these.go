c57410d5db1f11f6400931b6832ff6503e60267c
Benjamin Sanvoisin <benjamin.sanvoisin@gmail.com>
2024-10-21 16:08:23 +0200

    feat(0.3.0): new check

--- checks/resource_naming_this_these.go
+++ checks/resource_naming_this_these.go
@@ -10,7 +10,7 @@ import (
 func ResourceNamingThisThese(modules map[string]data.TerraformModule) (data.Check, error) {
 	dataCheck := data.Check{
 		ID:                "TF_NAM_001",
-		Name:              "Resources and datasources in modules should be named \"this\" or \"these\" if their type is unique",
+		Name:              "Resources in modules should be named \"this\" or \"these\" if their type is unique",
 		RelatedGuidelines: "https://padok-team.github.io/docs-terraform-guidelines/terraform/terraform_naming.html#resource-andor-data-source-naming",
 		Status:            "✅",
 	}
@@ -49,36 +49,6 @@ func ResourceNamingThisThese(modules map[string]data.TerraformModule) (data.Chec
 							Path:        resource.Pos.Filename,
 							LineNumber:  resource.Pos.Line,
 							Description: resource.Type + " - " + resource.Name,
-						})
-					}
-				}
-			}
-		}
-		// Check data sources
-		for _, dataResource := range moduleConf.DataResources {
-			// Check if the type of the resource is unique within the module
-			numberOfSameType := 0
-			for _, res := range moduleConf.DataResources {
-				if res.Type == dataResource.Type {
-					numberOfSameType++
-				}
-			}
-			// If there is only one instance of this type of resource, check if its named this or these (If they create more than 1 with a for each)
-			if numberOfSameType == 1 {
-				if pluralize.IsPlural(dataResource.Name) {
-					if dataResource.Name != "these" {
-						resourcesInError = append(resourcesInError, data.Error{
-							Path:        dataResource.Pos.Filename,
-							LineNumber:  dataResource.Pos.Line,
-							Description: dataResource.Type + " - " + dataResource.Name,
-						})
-					}
-				} else {
-					if dataResource.Name != "this" {
-						resourcesInError = append(resourcesInError, data.Error{
-							Path:        dataResource.Pos.Filename,
-							LineNumber:  dataResource.Pos.Line,
-							Description: dataResource.Type + " - " + dataResource.Name,
 						})
 					}
 				}
